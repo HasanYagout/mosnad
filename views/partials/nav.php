47af44e324b33d85320ade653635064ca53d821d
Hasan Yagout <yagouthasan@gmail.com>
2024-08-26 13:10:38 +0300

    first commit

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -11,13 +11,7 @@
                 <li class="nav-item py-3"><a class="nav-link  text-primary text-hover-secondary fs-5" href="news.php">News</a></li>
                 <li class="nav-item py-3"><a class="nav-link text-primary text-hover-secondary fs-5" href="contact.php">Contact Us</a></li>
                 <li class="nav-item py-3"><a class="nav-link text-primary text-hover-secondary fs-5" href="/posts">Posts</a></li>
-                <?php if ($_SESSION['user'] ?? false) : ?>
-                    <li class="nav-item py-3"><a class="nav-link text-primary text-hover-secondary fs-5" id="login" href="/logout">Log Out</a></li>
 
-                <?php else : ?>
-                <li class="nav-item py-3"><a class="nav-link text-primary text-hover-secondary fs-5" id="login" href="/register">Register</a></li>
-                <li class="nav-item py-3"><a class="nav-link text-primary text-hover-secondary fs-5" id="login" href="/login">Login</a></li>
-                <?php endif; ?>
             </ul>
 
         </div>
